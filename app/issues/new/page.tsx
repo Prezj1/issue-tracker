9145960b5ee1efe53381ee3a046170bcf6d5dc53
PrezJ1 <joymakasi@hotmail.co.uk>
2023-10-24 18:36:46 +0100

    impliment client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,24 +1,28 @@
 "use client";
 
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import { createIssueSchema } from "@/app/validatrionSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
-const newIssuePage = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+const NewIssuePage = () => {
   const router = useRouter();
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -42,6 +46,11 @@ const newIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -49,10 +58,15 @@ const newIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
   );
 };
 
-export default newIssuePage;
+export default NewIssuePage;
