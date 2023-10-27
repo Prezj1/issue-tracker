38c55f2783840a10ba10a5b68d3e56ee7333145f
PrezJ1 <joymakasi@hotmail.co.uk>
2023-10-27 13:59:59 +0100

    Building an assignee component

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -9,12 +9,14 @@ import { useSession } from "next-auth/react";
 import {
   Avatar,
   Box,
+  Button,
   Container,
   DropdownMenu,
   Flex,
   Text,
 } from "@radix-ui/themes";
 import { Skeleton } from "@/app/components";
+import { CaretDownIcon } from "@radix-ui/react-icons";
 
 const NavBar = () => {
   return (
@@ -63,38 +65,44 @@ const NavLinks = () => {
 
 const AuthStatus = () => {
   const { status, data: session } = useSession();
+  console.log("status", status);
 
   if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated")
     return (
       <Link className="nav-link" href="/api/auth/signin">
-        Login
+        <Button color="sky" className="hover:cursor">
+          Login
+        </Button>
       </Link>
     );
 
   return (
-    <Box>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger>
-          <Avatar
-            src={session!.user!.image!}
-            fallback="?"
-            size="2"
-            radius="full"
-            className="cursor-pointer"
-          />
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content>
-          <DropdownMenu.Label>
-            <Text size="2">{session!.user!.email}</Text>
-          </DropdownMenu.Label>
-          <DropdownMenu.Item>
-            <Link href="/api/auth/signout">Log out</Link>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
-    </Box>
+    <div>
+      <Box>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session!.user!.image!}
+              fallback="?"
+              size="2"
+              radius="full"
+              className="cursor-pointer"
+              referrerPolicy="no-referrer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session!.user!.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">Log out</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Box>
+    </div>
   );
 };
 
